b302b5be5a739c74efb0c711d202f3a687275753
kakot <b2212620@photon.chitose.ac.jp>
2022-11-07 17:29:15 +0900

    3-1まで

---
+++ Prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,10 @@
+package lecture03;
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
