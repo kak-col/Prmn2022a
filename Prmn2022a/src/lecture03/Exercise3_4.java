308be1efe97851205a88482c3671ea1271a38600
kakot <b2212620@photon.chitose.ac.jp>
2022-11-14 16:57:42 +0900

    課題4まで

--- Prmn2022a/src/lecture03/Exercise3_4.java
+++ Prmn2022a/src/lecture03/Exercise3_4.java
@@ -5,12 +5,9 @@ import java.util.ArrayList;
 public class Exercise3_4 {
     public static void main(String[] args) {
         ArrayList<Vegetable> vegetables = new ArrayList<Vegetable>();
-        Vegetable vegetable1 = new Vegetable("にんじん",117);
-        vegetables.add(vegetable1);
-        Vegetable vegetable2 = new Vegetable("じゃがいも",120);
-        vegetables.add(vegetable2);
-        Vegetable vegetable3 = new Vegetable("たまねぎ",154);
-        vegetables.add(vegetable3);
+        vegetables.add(new Vegetable("にんじん",117));
+        vegetables.add(new Vegetable("じゃがいも",120));
+        vegetables.add(new Vegetable("たまねぎ",154));
 
         for (Vegetable vegetable : vegetables) {
             vegetable.print();
